fde37c04925f09e3fc055d8c8f0445eb682bc983
Vladimir Alekseychuk <alekseychuk972mail.ru@gmail.com>
2019-11-16 15:30:54 +0200

    Updates

---
+++ precache-manifest.2e98c5e4a3b37b711fb8f22cee4c7c87.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa78f82e682d1ffa9c",
+    "url": "/GoRestApplication/static/css/main.cc519b79.chunk.css"
+  },
+  {
+    "revision": "72aa78f82e682d1ffa9c",
+    "url": "/GoRestApplication/static/js/main.78b7d141.chunk.js"
+  },
+  {
+    "revision": "7ff26fb90d3412381e2d",
+    "url": "/GoRestApplication/static/js/runtime~main.26198a71.js"
+  },
+  {
+    "revision": "80dd56b85446f91ee101",
+    "url": "/GoRestApplication/static/js/2.3ea79965.chunk.js"
+  },
+  {
+    "revision": "ac7839172cc93cd23cd9",
+    "url": "/GoRestApplication/static/js/3.4403214b.chunk.js"
+  },
+  {
+    "revision": "039be0ddbd6081432cd5",
+    "url": "/GoRestApplication/static/js/4.3a627214.chunk.js"
+  },
+  {
+    "revision": "f76534d3dfa931000f7c",
+    "url": "/GoRestApplication/static/js/5.493dc20c.chunk.js"
+  },
+  {
+    "revision": "888cb1244a7f0ba1dd87",
+    "url": "/GoRestApplication/static/css/6.ff0d6dda.chunk.css"
+  },
+  {
+    "revision": "888cb1244a7f0ba1dd87",
+    "url": "/GoRestApplication/static/js/6.3c44e9d8.chunk.js"
+  },
+  {
+    "revision": "a474181993d51c106041",
+    "url": "/GoRestApplication/static/css/7.8ea45a78.chunk.css"
+  },
+  {
+    "revision": "a474181993d51c106041",
+    "url": "/GoRestApplication/static/js/7.e174a06d.chunk.js"
+  },
+  {
+    "revision": "54736b5bd0ea222bf4b5",
+    "url": "/GoRestApplication/static/js/8.16d61a52.chunk.js"
+  },
+  {
+    "revision": "1b751743094d38b6d951",
+    "url": "/GoRestApplication/static/js/9.60aba8f0.chunk.js"
+  },
+  {
+    "revision": "1cce7134d157f0f52dc95a65250c568a",
+    "url": "/GoRestApplication/index.html"
+  }
+];
